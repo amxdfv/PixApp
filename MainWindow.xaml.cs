dbea1f62285bf8b3b534603393371403060984e2
A.Morozov <42004560+amxdfv@users.noreply.github.com>
2020-04-19 17:32:10 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap picture = Properties.Resources.volvo;
+        Bitmap picture_fin = Properties.Resources.volvo;
+
+        private void Button1_Click(object sender, RoutedEventArgs e)  // загружаем файл
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+
+            OFD.Filter = "Images (*.jpg)|*.jpg";
+            bool? result = OFD.ShowDialog();
+            if (result == true)
+            {
+              
+                picture = new Bitmap(Bitmap.FromFile(OFD.FileName));
+            }
+
+
+                System.Windows.Media.Imaging.BitmapSource b =
+    System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+           picture.GetHbitmap(),
+           IntPtr.Zero,
+           Int32Rect.Empty,
+           BitmapSizeOptions.FromEmptyOptions());
+            Image1.Source = b;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)  // запускаем преобразование
+        {
+            int itr = 1;
+            try
+            {
+                itr = Int32.Parse(TextBox1.Text);
+            }catch (System.FormatException sfe)
+            {
+                MessageBox.Show("Введи номальное число!","Ну что ж ты так!!!", MessageBoxButton.OK);
+            }
+            
+            PixPrc PP = new PixPrc();
+            Bitmap pix_fin = PP.ImgProc(picture,itr);
+            System.Windows.Media.Imaging.BitmapSource b =
+    System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+           pix_fin.GetHbitmap(),
+           IntPtr.Zero,
+           Int32Rect.Empty,
+           BitmapSizeOptions.FromEmptyOptions());
+            Image2.Source = b;
+
+            picture_fin = pix_fin;
+           
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)   // сохраняем файл
+        {
+            SaveFileDialog SFD = new SaveFileDialog();
+
+            SFD.Filter = "Images (*.jpg)|*.jpg";
+         
+            bool? result = SFD.ShowDialog();
+            if (result == true) {
+                string path = SFD.FileName;
+
+                try
+                {
+                    picture_fin.Save(path);
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+            
+            
+        }
+    }
+}
---
+++ PixPrc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixApp
+{
+    class PixPrc
+    {
+
+        public Bitmap ImgProc(Bitmap picture, int itr)
+        {
+            
+            Bitmap pic_one = picture;
+            
+
+            for (int i = 0; i < itr; i = i + 1)
+            {
+                int height = pic_one.Height;
+                int width = pic_one.Width;
+                Bitmap pic_f = new Bitmap(width / 2, height / 2);
+                for (int y = 0; y < height / 2; y = y + 1)
+                {
+                    for (int x = 0; x < width / 2; x = x + 1)
+                    {
+                        int x1 = 2 * x;
+                        int y1 = 2 * y;
+                        int r = (pic_one.GetPixel(x1, y1).R + pic_one.GetPixel(x1 + 1, y1).R
+                            + pic_one.GetPixel(x1, y1 + 1).R + pic_one.GetPixel(x1 + 1, y1 + 1).R) / 4;
+                        int g = (pic_one.GetPixel(x1, y1).G + pic_one.GetPixel(x1 + 1, y1).G
+                            + pic_one.GetPixel(x1, y1 + 1).G + pic_one.GetPixel(x1 + 1, y1 + 1).G) / 4;
+                        int b = (picture.GetPixel(x1, y).B + pic_one.GetPixel(x1 + 1, y).B
+                            + pic_one.GetPixel(x1, y1 + 1).B + pic_one.GetPixel(x1 + 1, y1 + 1).B) / 4;
+                        int a = (picture.GetPixel(x1, y).A + pic_one.GetPixel(x1 + 1, y).A
+                            + pic_one.GetPixel(x1, y1 + 1).A + pic_one.GetPixel(x1 + 1, y1 + 1).A) / 4;
+                        Color clr = Color.FromArgb(a, r, g, b);
+                        pic_f.SetPixel(x, y, clr);
+                    }
+                }
+                pic_one = pic_f;
+            }
+            return pic_one;
+
+        }
+
+    }
+}
